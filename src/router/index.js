8bf2cf69cf369e04ff5436e5b8a11502a57c364a
vadim <vadim.tatochko98@mail.ru>
2021-02-15 20:52:17 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,69 +1,65 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Login',
-    name: 'Login',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/Profile',
-    name: 'Profile',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Profile.vue')
-  },
-  {
-    path: '/Register',
-    name: 'Register',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/Categories',
-    name: 'Categories',
-    meta: { layout: 'main' },
-    component: () => import ('../views/Categories.vue')
-  },
-  {
-    path: '/History',
-    name: 'History',
-    meta: { layout: 'empty' },
-    component: () => import('../views/History.vue')
-  },
-  {
-    path: '/Record',
-    name: 'Record',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Record.vue')
-  },
-  {
-    path: '/Detail',
-    name: 'Detail',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Detail.vue')
-  },
-  {
-    path: '/Planning',
-    name: 'Planning',
-    meta: { layout: 'empty' },
-    component: () => import('../views/Planning.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      meta: { layout: 'main' },
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/Profile',
+      name: 'Profile',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Profile.vue')
+    },
+    {
+      path: '/Register',
+      name: 'Register',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Register.vue')
+    },
+    {
+      path: '/Categories',
+      name: 'Categories',
+      meta: { layout: 'main' },
+      component: () => import ('../views/Categories.vue')
+    },
+    {
+      path: '/History',
+      name: 'History',
+      meta: { layout: 'empty' },
+      component: () => import('../views/History.vue')
+    },
+    {
+      path: '/Record',
+      name: 'Record',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Record.vue')
+    },
+    {
+      path: '/Detail',
+      name: 'Detail',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Detail.vue')
+    },
+    {
+      path: '/Planning',
+      name: 'Planning',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Planning.vue')
+    }
+  ]
 })
-
-export default router
